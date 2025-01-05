074f1c556b1ef27b20a439b38d407e6dadc52931
edacank <edacan1924@gmail.com>
2025-01-05 21:17:08 +0300

    Summary
    Refactoring methods

--- Royal-Run/Assets/Scripts/LevelGenerator.cs
+++ Royal-Run/Assets/Scripts/LevelGenerator.cs
@@ -8,22 +8,36 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] int startingChunksAmount = 12;
     [SerializeField] Transform chunkParent;
     [SerializeField] float chunkLength = 10f;
+    GameObject[] chunks = new GameObject[12];
     void Start()
     {
-        for(int i =0; i< startingChunksAmount; i++)
+        SpawnChunks();
+
+    }
+
+    private void SpawnChunks()
+    {
+        for (int i = 0; i < startingChunksAmount; i++)
         {
-            float spawnPositionZ;
-            if(i == 0)
-            {
-                spawnPositionZ = transform.position.z;
-            }
-            else
-            {
-                spawnPositionZ = transform.position.z +(i*chunkLength);
-            }
-            Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y,spawnPositionZ);
-            Instantiate(chunkPrefab, chunkSpawnPos,  Quaternion.identity, chunkParent);
+            float spawnPositionZ = CalculateSpawnPositionZ(i);
+            Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
+            GameObject newChunk=  Instantiate(chunkPrefab, chunkSpawnPos, Quaternion.identity, chunkParent);
+            chunks[i] = newChunk;
         }
-        
+    }
+
+    private float CalculateSpawnPositionZ(int i)
+    {
+        float spawnPositionZ;
+        if (i == 0)
+        {
+            spawnPositionZ = transform.position.z;
+        }
+        else
+        {
+            spawnPositionZ = transform.position.z + (i * chunkLength);
+        }
+
+        return spawnPositionZ;
     }
 }
