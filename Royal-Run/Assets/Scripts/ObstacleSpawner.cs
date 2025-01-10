f883f085d8ce4b965ff571388f3d5888e85c06eb
edacank <edacan1924@gmail.com>
2025-01-10 23:07:17 +0300

    Summary
    New prefabs are added.

--- Royal-Run/Assets/Scripts/ObstacleSpawner.cs
+++ Royal-Run/Assets/Scripts/ObstacleSpawner.cs
@@ -8,6 +8,7 @@ public class ObstacleSpawner : MonoBehaviour
     [SerializeField] float obstacleSpawnTime = 1f;
     //int obstacleSpawned = 0;
     [SerializeField] Transform obstacleParent;
+    [SerializeField] float spawnWidth = 4f;
     void Start()
     {
         StartCoroutine(SpawnObstacleRoutine());
@@ -18,6 +19,7 @@ public class ObstacleSpawner : MonoBehaviour
         while(true)
         {
             GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnWidth,+spawnWidth), transform.position.y,transform.position.z);
             yield return new WaitForSeconds(obstacleSpawnTime);
         Instantiate(obstaclePrefab,transform.position,Random.rotation,obstacleParent);
       //  obstacleSpawned++;
