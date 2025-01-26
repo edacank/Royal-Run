bd8c4a5f4889c6b4f9b0feb6d3acbc0b1ce3aec8
edacank <edacan1924@gmail.com>
2025-01-26 23:54:42 +0300

    sum
    .

--- Royal-Run/Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Royal-Run/Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+   [SerializeField] Animator animator;
+   [SerializeField] float collisionCooldown =1f;
+
+   const string hitString ="Hit";
+   float cooldownTimer = 0f;
+   void Update()
+   {
+      cooldownTimer += Time.deltaTime;
+   }
    void OnCollisionEnter(Collision other){
 
-    Debug.Log(other.gameObject.name);
+         if(cooldownTimer < collisionCooldown) return;
+         animator.SetTrigger(hitString);
+         cooldownTimer = 0f;
    }
 }
